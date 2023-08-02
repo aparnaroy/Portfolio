584119ba269d9f770e07f1eca2ad33c0fde32620
Aparna Roy <aparnar@udel.edu>
2023-08-02 09:10:55 -0400

    updated redeploy-site.sh script

--- ~/redeploy-site.sh
+++ ~/redeploy-site.sh
@@ -6,10 +6,8 @@ cd ./Portfolio
 # Fetch latest changes from main branch on GitHub and reset local repository
 git fetch && git reset origin/main --hard
 
-# Activate Python virtual environment and install dependencies
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+# Spin containers down to prevent out of memory issues on our VPS instances when building in the next step
+docker compose -f docker-compose.prod.yml down
 
-# Restart myportfolio service
-systemctl daemon-reload
-systemctl restart myportfolio
+# Build the container
+docker compose -f docker-compose.prod.yml up -d --build
